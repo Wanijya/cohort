8233a67a1967db236dbfd4e11b07e71e1e39d9ad
Wanijya <wanijyabisen@gmail.com>
2025-02-27 02:47:52 +0530

    Convert "50" (string) into a number using 3 different methods.

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Variables & Data Types (5 Questions)
-
-//--> 6. Declare a const object, modify its properties, and log the updated object.
-const obj = {
-    name: "Wanijya",
-    age: 22,
-    email: "test@test.com"
-}
-// Object.freeze(obj);
-obj.age = 23;
+//--> 7. Convert "50" (string) into a number using 3 different methods.
+console.log(Number("50"));
+console.log(parseInt("50"));
+console.log(+"50");
