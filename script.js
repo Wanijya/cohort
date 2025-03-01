339e13454d7629a12e1dfbafd0a39ad22c72a608
Wanijya <wanijyabisen@gmail.com>
2025-03-01 20:04:12 +0530

    Sort an array in descending order.

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-// 17. Find the second largest number in an array.
+// 18. Sort an array in descending order.
 
-let arr = [
-  1, 5, 9, 3, 4, 7, 2, 1, 6, 13, 67, 12, 4, 9, 21, 13, 21, 5, 55, 58, 55, 58,
-];
+let numbers = [5, 3, 8, 1, 9];
 
-// unique values
-// let uniqueArr = [...new Set(arr)];
-
-// // sort in descending order
-// let sortedArr = uniqueArr.sort(function (a, b) {
-//   return b - a;
-// })[1];
-
-// console.log(sortedArr);
-
-console.log(
-  [...new Set(arr)].sort(function (a, b) {
-    return b - a;
-  })[1]
-);
+let sortArr = numbers.sort(function (a, b) {
+  return b - a;
+});
+console.log(sortArr);
