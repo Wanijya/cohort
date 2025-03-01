7b1cec61311b3b2a9b0f88a089ac5bd0faa7e120
Wanijya <wanijyabisen@gmail.com>
2025-03-01 18:35:35 +0530

    Write a for loop to print numbers from 10 to 1 in reverse.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// 9. Create an array of 5 numbers and log the sum using .reduce().
-var arr = [1, 2, 3, 4, 5];
-var ans = arr.reduce(function (acc, val) {
-  return acc + val;
-}, 0);
-console.log(ans);
+// 11. Write a for loop to print numbers from 10 to 1 in reverse.
+
+for (var i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+let val = 10;
+while (val > 0) {
+  console.log(val);
+  val--;
+}
