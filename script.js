d5d8559b50be828ffc0a9545ef59831f77655fd9
Wanijya <wanijyabisen@gmail.com>
2025-03-01 18:22:28 +0530

    Create an array of 5 numbers and log the sum using .reduce().

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-//--> 10. Explain the difference between undefined, null, and NaN with examples.
-
-var a;
-console.log(a);
-
-var a = null;
-console.log(a);
-
-var a = "Wanijya" / 2;
-console.log(a);
+// 9. Create an array of 5 numbers and log the sum using .reduce().
+var arr = [1, 2, 3, 4, 5];
+var ans = arr.reduce(function (acc, val) {
+  return acc + val;
+}, 0);
+console.log(ans);
