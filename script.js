f3f37d35b02e6debff169e7caa86dc1e48d9e837
Wanijya <wanijyabisen@gmail.com>
2025-03-01 20:01:05 +0530

    Find the second largest number in an array.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // 17. Find the second largest number in an array.
 
-let arr = [1,5,9,3,4,7,2,1,6,13,67,12,4,9,21,13,21,5,55,58,55,58];
+let arr = [
+  1, 5, 9, 3, 4, 7, 2, 1, 6, 13, 67, 12, 4, 9, 21, 13, 21, 5, 55, 58, 55, 58,
+];
 
 // unique values
-let uniqueArr = [...new Set(arr)];
+// let uniqueArr = [...new Set(arr)];
 
-// sort in descending order
-let sortedArr = uniqueArr.sort(function (a,b){
-  return b-a;
-})
+// // sort in descending order
+// let sortedArr = uniqueArr.sort(function (a, b) {
+//   return b - a;
+// })[1];
 
-console.log(sortedArr);
+// console.log(sortedArr);
+
+console.log(
+  [...new Set(arr)].sort(function (a, b) {
+    return b - a;
+  })[1]
+);
