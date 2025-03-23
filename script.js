a826406b0596300ff67469b5a150a64fd8cd2ed3
Wanijya <wanijyabisen@gmail.com>
2025-03-24 00:54:08 +0530

    Circumference of circle

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-// WAP to calculate area of triangle using Heron's formula
+// WAP to calculate Circumference of circle
+// CF = 2πR;
 
-let a = Number(prompt("Enter the first side of the triangle: "));
-let b = Number(prompt("Enter the second side of the triangle: "));
-let c = Number(prompt("Enter the third side of the triangle: "));
+let radius = Number(prompt("Circumference of Circle"));
 
-/*
- heros formula = sqrt of s * (s-a) * (s-b) * (s-c)
-  where s = (a+b+c)/2
-*/
-if (a + b <= c || a + c <= b || c + b <= a) {
-  console.log("Triangle is not possible with the given sides");
-} else {
-  let s = (a + b + c) / 2;
-  console.log(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
-}
+console.log(2 * Math.PI * radius); // (property) Math.PI: number
