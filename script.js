7675a5e60b709564c2cb335abbba00f6d0b84185
Wanijya <wanijyabisen@gmail.com>
2025-03-24 00:40:00 +0530

    area of triangle using Heron's formula

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// WAP to make a OTP equation (4 digits)
-console.log(Math.floor(Math.random() * 9000 + 1000));
+// WAP to calculate area of triangle using Heron's formula
 
+let a = Number(prompt("Enter the first side of the triangle: "));
+let b = Number(prompt("Enter the second side of the triangle: "));
+let c = Number(prompt("Enter the third side of the triangle: "));
 
+/*
+ heros formula = sqrt of s * (s-a) * (s-b) * (s-c)
+  where s = (a+b+c)/2
+*/
+if (a + b <= c || a + c <= b || c + b <= a) {
+  console.log("Triangle is not possible with the given sides");
+} else {
+  let s = (a + b + c) / 2;
+  console.log(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+}
