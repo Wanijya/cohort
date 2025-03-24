c584e2ccb897cfbbca414e82d9d77e8c653626dd
Wanijya <wanijyabisen@gmail.com>
2025-03-24 13:18:58 +0530

    accept name and age from the user. Check if the user is valid voter or not.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// WAP to accept an int and check whether it is an even or odd number.
+// WAP accept name and age from the user. Check if the user is valid voter or not.
 
-let num = Number(prompt("Enter a number: "));
+let age = Number(prompt("Enter a age:"));
+let username = prompt("Enter your name:");
 
-if (num % 2 == 0) {
-  console.log(`${num} is an even number.`);
+if (age >= 18) {
+  console.log(`Hello ${username}, you are eligible to vote.`);
+} else if (age < 18) {
+  console.log(`Hello ${username}, you are not eligible to vote.`);
 } else {
-  console.log(`${num} is an odd number.`);
+  console.log("Invalid input");
 }
