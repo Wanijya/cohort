d0b8f4b1709556bd5aa20a91fb72027762448392
Wanijya <wanijyabisen@gmail.com>
2025-04-02 01:12:51 +0530

    Leap year

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-// WAP to check the year is leap year or not.
-
 let year = Number(prompt("Enter a year: "));
+let isLeapYear = false;
 
-if (year % 4 == 0) {
-  if (year % 100 == 0) {
-    if (year % 400 == 0) {
-      console.log("Leap year");
-    } else {
-      console.log("Not a leap year");
-    }
-  } else {
-    console.log("Leap year");
-  }
-} else {
-  console.log("Not a leap year");
-}
+if (year % 4 == 0 && year % 100 != 0) {
+    console.log("leap year");
+} else if (year % 400 == 0) {
+    console.log("leap year");
+} else console.log("not a leap year");
