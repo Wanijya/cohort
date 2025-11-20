7a5793d11c080744d950d81806a29b73cb82616c
Wanijya <wanijyabisen@gmail.com>
2025-11-20 16:00:19 +0530

    How to use Middleaware

--- server.js
+++ server.js
@@ -1,48 +1,5 @@
-const express = require("express");
-const connectToDB = require("./src/db/db");
-const noteModel = require("./src/models/note.model");
+const app = require("./src/app");
 
-connectToDB();
-const app = express();
-app.use(express.json());
-
-app.post("/notes", async (req, res) => {
-  const { title, content } = req.body;
-  console.log(title, content);
-  await noteModel.create({ title, content });
-  res.json({ message: "Note created successfully" });
-});
-
-app.get("/notes", async (req, res) => {
-  const notes = await noteModel.find();
-
-  res.json({ message: "Notes Fatch successfully", notes });
-});
-
-app.delete("/notes/:id", async (req, res) => {
-  const noteId = req.params.id;
-
-  await noteModel.findOneAndDelete({
-    _id: noteId,
-  });
-  res.json({ message: "Note deleted successfully" });
-});
-
-app.patch("/notes/:id", async (req, res) => {
-  const noteId = req.params.id;
-  const { title } = req.body;
-
-  await noteModel.findOneAndUpdate(
-    {
-      _id: noteId,
-    },
-    {
-      title: title,
-    }
-  );
-  res.json({ message: "Note updated successfully" });
-});
-
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
---
+++ src/routes/index.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log("this middleaware is between router and api");
+  next();
+});
+
+router.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+
+module.exports = router;
