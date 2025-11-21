2cca19fa2fb7c8adcc81584e57f6c82c0987bd3c
Wanijya <wanijyabisen@gmail.com>
2025-11-21 16:41:29 +0530

    Authentication with JWT token

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 const express = require("express");
-const indexRoutes = require("./routes/index.routes");
+const authRoutes = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log("this middleaware is between app and router");
-  next();
-});
-
-app.use("/", indexRoutes);
+app.use("/auth", authRoutes);
 
 module.exports = app;
